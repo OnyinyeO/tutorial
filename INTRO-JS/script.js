ff975aab1afe9c85d9a7c28389a53cb4fc6f1e4c
Onyinye Ochi <Osemene.go@gmail.com>
2022-03-26 00:24:49 +0100

    Added Card folder to Tutorial folder

---
+++ INTRO-JS/script.js
@@ -0,0 +1,24 @@
+let user_time = 30;
+const userscore = 100;
+let user_time2 = 30;
+
+let greetings ="welcome to my site"
+
+let my_bool = true
+let my_bool2 = false
+
+
+
+// Datatype
+// 1. Number
+// 2. Stings
+// 3. boolean
+// 4. symbols
+// 5. Null
+// 6. Undefined
+
+
+
+
+
+
