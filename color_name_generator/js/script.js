69cea75679a5689f301817301d9f0f6a213f62e4
Onyinye Ochi <Osemene.go@gmail.com>
2022-04-28 20:26:58 +0100

    color

--- color_name_generator/js/script.js
+++ color_name_generator/js/script.js
@@ -1,20 +1,31 @@
 // color generator function
 const colorGenerator = (color) => {
-
-    if (color) {
-        displayColor(color);
-    }
-    else {
-        const randomColor = Math.floor(Math.random()* 16777215).toString(16);
-        displayColor(randomColor);    
-        }
-    }
-
+  if (color) {
+    displayColor(color);
+  } else {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    displayColor(randomColor);
+  }
+};
 
 // display color function
-const displayColor = () => {
-    let name_value = ntc.name(color);
-    console.log(name_value);
+const displayColor = (color) => {
+  let name_value = ntc.name(color);
+  console.log(name_value);
 
     document.body.style.backgroundColor = name_value[0];
+    colorInput.value = name_value[0];
+    colorName.innerHTML = name_value[1];
+    hexValue.innerHTML = name_value[0];
+};
+//Buttons Events
+genSearch.addEventListener("click", function (e) {
+    colorGenerator(colorInput.value);
 }
+)
+genBtn.addEventListener("click", function (e) {
+    colorGenerator();
+}
+)
+
+colorGenerator();
